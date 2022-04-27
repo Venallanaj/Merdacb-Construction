ad1c156fdfc2e207e280a696e8c31be9bb8c2904
vena <vena.llanaj@gmail.com>
2022-04-27 20:40:58 +0200

    Add tls

--- config/mail.php
+++ config/mail.php
@@ -38,7 +38,7 @@ return [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.titan.email'),
             'port' => env('MAIL_PORT', 465),
-            'encryption' => env('MAIL_ENCRYPTION', 'SSL'),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
