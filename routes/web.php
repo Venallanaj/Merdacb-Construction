9c6e81c6a804dceec31d5c5d267bd0bbd5647178
vena <vena.llanaj@gmail.com>
2022-04-29 00:18:50 +0200

    add changes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('contact','App\Http\Controllers\ContactController@contact');  
-Route::post('contact','App\Http\Controllers\ContactController@contactPost');  
+Route::get('/contact', 'ContactController@contact')->name('contact');
+Route::post('/contact', 'ContactController@contactPost')->name('contactPost');
+// Route::get('contact','App\Http\Controllers\ContactController@contact');  
+// Route::post('contact','App\Http\Controllers\ContactController@contactPost');  
 
 
 Route::get('/', function () {
